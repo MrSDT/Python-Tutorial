7f0346669c59b14e651be88c9b769d0420faed24
MrSDT <55284466+MrSDT@users.noreply.github.com>
2021-04-29 21:27:49 +0430

    Add files via upload

---
+++ print.py
@@ -0,0 +1,11 @@
+print('Hello World With Single Quote') # Single Quote
+print("------------------------------")
+print("Hello World With Doubble Quote") # Doubble Quote
+print("------------------------------")
+print("Hello World " + 'With Both Quote') # Both Quote & +
+print("------------------------------")
+print("Hello World To " + "10") # + With Number in String
+print("------------------------------")
+print("Im Hello\n" + "World") # Next Line
+print("------------------------------")
+print("Hello World Says: \"I'm Alone\"") # Doubble Quote in Doubble Quote
