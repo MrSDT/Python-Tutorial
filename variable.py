41ae73c0b271ccc82458f9673f531e3d82434713
MrSDT <55284466+MrSDT@users.noreply.github.com>
2021-04-30 21:59:56 +0430

    Add files via upload

---
+++ variable.py
@@ -0,0 +1,25 @@
+variable = "Hello World" # Seting Variable with String Type
+mamad = 55 # Seting Variable with Int Type
+print(f"My name is: {variable} and my age is: {mamad}") # Printing Variable with F String
+print("------------------------------------------")
+# Calling Variable Type
+print(type(variable))
+print(type(mamad))
+print("------------------------------------------")
+# Variable is Case Sensitiv
+a = "Hello"
+A = "World"
+print(A)
+print("------------------------------------------")
+# Multiple Variables & Values in one Line
+var1 , var2 , var3 = "Mamad" , "Hasan" , "Hossein"
+print(var1)
+print(var2)
+print(var3) # Or You can use "print(var1 , var2 , var3)"
+print("------------------------------------------")
+# Multiple Variable With One Value
+var1 = var2 = var3 = "Hello World"
+print(var1)
+print(var2)
+print(var3)
+print("------------------------------------------")
